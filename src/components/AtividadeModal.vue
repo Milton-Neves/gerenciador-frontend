2bc72fd53bec33e0d7ebe9cde1b4a308272d1d06
milton.neto <milton.neto@parnamirim.rn.gov.br>
2024-05-20 15:03:08 -0300

    second commit

--- src/components/AtividadeModal.vue
+++ src/components/AtividadeModal.vue
@@ -1,115 +1,62 @@
 <template>
-  <!-- <div v-if="show" class="modal">
+  <div v-if="show" class="modal">
     <div class="modal-content">
-      <span class="close" @click="$emit('close')">&times;</span>
-      <h2>Nova Atividade</h2>
+      <div class="close">
+        <h2>Nova Atividade</h2>
+        <span class="close" @click="$emit('close')">&times;</span>
+      </div>
       <form @submit.prevent="salvarAtividade">
-        <div class="form-group">
-          <label for="nome">Nome:</label>
-          <input type="text" id="nome" v-model="novaAtividade.nome" required />
+        <div class="teste">
+          <div class="form-group">
+            <label for="dataInicio">Data de Início:</label>
+            <input type="date" id="dataInicio" v-model="novaAtividade.dataInicio" required />
+          </div>
+          <div class="form-group">
+            <label for="dataFim">Data de Conclusão:</label>
+            <input type="date" id="dataFim" v-model="novaAtividade.dataFim" required />
+          </div>
         </div>
+        <div class="teste">
+          <div class="form-group">
+            <label for="status">Status:</label>
+            <select id="status" v-model="novaAtividade.status" required>
+              <option value="">Informe o status</option>
+              <option value="Pendente">Pendente</option>
+              <option value="Revisada">Revisada</option>
+              <option value="Execução">Execução</option>
+              <option value="Concluída">Concluída</option>
+            </select>
+          </div>
+          <!-- <div class="form-group">
+            <label for="projeto">Projeto:</label>
+            <select id="projeto" v-model="novaAtividade.nomeProjeto" required>
+              <option value="">Selecione um projeto</option>
+              <option v-for="projeto in projetos" :key="projeto.id" :value="projeto.nome">
+                {{ projeto.nome }}
+              </option>
+            </select>
+          </div> -->
+          <div class="form-group">
+            <label for="nomeProjeto">Selecionar Projeto</label>
+            <div class="select-wrapper">
+              <select class="form-control" v-model="nomeProjeto">
+                <option>Projeto 1</option>
+                <option>Projeto 2</option>
+                <option>Projeto 3</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
         <div class="form-group">
           <label for="descricao">Descrição:</label>
           <textarea id="descricao" v-model="novaAtividade.descricao" required></textarea>
         </div>
-        <div class="form-group">
-          <label for="projeto">Projeto:</label>
-          <select id="projeto" v-model="novaAtividade.nomeProjeto" required>
-            <option value="">Selecione um projeto</option>
-            <option v-for="projeto in projetos" :key="projeto.id" :value="projeto.nome">
-              {{ projeto.nome }}
-            </option>
-          </select>
+        <div class="form-group buttons">
+          <button type="button" class="btn btn-secondary" @click="closeModal">Cancelar</button>
+          <button type="submit" class="btn-salvar">Salvar</button>
         </div>
-        <div class="form-group">
-          <label for="status">Status:</label>
-          <select id="status" v-model="novaAtividade.status" required>
-            <option value="">Selecione um status</option>
-            <option value="Pendente">Pendente</option>
-            <option value="Revisada">Revisada</option>
-            <option value="Execução">Execução</option>
-            <option value="Concluída">Concluída</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="dataInicio">Data de Início:</label>
-          <input type="date" id="dataInicio" v-model="novaAtividade.dataInicio" required />
-        </div>
-        <div class="form-group">
-          <label for="dataFim">Data de Fim:</label>
-          <input type="date" id="dataFim" v-model="novaAtividade.dataFim" required />
-        </div>
-        <button type="submit" class="btn-salvar">Salvar</button>
       </form>
-    </div>
-  </div> -->
-
-  <div class="modal" :class="{ 'is-active': isActive }">
-    <div class="modal-background" @click="closeModal"></div>
-    <div class="modal-content">
-      <div class="box">
-        <!-- <button class="modal-close" @click="closeModal">&times;</button> -->
-        <!-- <h2>{{ isEditMode ? 'Editar Projeto' : 'Novo Projeto' }}</h2> -->
-        <span class="close" @click="$emit('close')">&times;</span>
-
-        <h2>Nova Atividade</h2>
-
-        <form @submit.prevent="salvarAtividade">
-          <!-- Primeira Linha: Nome do Projeto -->
-          <div class="teste">
-            <div class="form-group">
-              <label for="dataInicio">Data de Início:</label>
-              <input type="date" id="dataInicio" v-model="novaAtividade.dataInicio" required />
-            </div>
-            <div class="form-group">
-              <label for="dataFim">Data de Conclusão:</label>
-              <input type="date" id="dataFim" v-model="novaAtividade.dataFim" required />
-            </div>
-          </div>
-          <div class="teste2">
-            <div class="form-group">
-              <label for="status">Status:</label>
-              <select id="status" v-model="novaAtividade.status" required>
-                <option value="">Informe o status</option>
-                <option value="Pendente">Pendente</option>
-                <option value="Revisada">Revisada</option>
-                <option value="Execução">Execução</option>
-                <option value="Concluída">Concluída</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="projeto">Nome do Projeto:</label>
-              <select id="projeto" v-model="novaAtividade.nomeProjeto" required>
-                <option value="projeto.nome">Selecione um projeto</option>
-                <option v-for="projeto in projetos" :key="projeto.id" :value="projeto.nome">
-                  {{ projeto.nome }}
-                </option>
-              </select>
-            </div>
-          </div>
-          <!-- Segunda Linha: Selecionar Cliente e Status do Projeto -->
-
-          <!-- Terceira Linha: Descrição -->
-          <div class="form-group">
-            <label for="descricao">Descrição</label>
-            <textarea
-              type="text"
-              rows="3"
-              class="form-control"
-              v-model="novaAtividade.descricao"
-              placeholder="Descreva sobre a atividade"
-            ></textarea>
-          </div>
-          <!-- Botões de Salvar e Cancelar -->
-          <div class="form-group buttons">
-            <button type="button" class="btn btn-secondary" @click="closeModal">Cancelar</button>
-            <!-- <button type="submit" class="btn btn-primary">
-              {{ isEditMode ? 'Salvar' : 'Adicionar' }}
-            </button> -->
-            <button type="submit" class="btn-salvar">Salvar</button>
-          </div>
-        </form>
-      </div>
     </div>
   </div>
 </template>
@@ -124,10 +71,10 @@ export default defineComponent({
       type: Boolean,
       required: true
     },
-    atividades: {
-      type: Array as PropType<Array<{ id: number; nome: string; descricao: string }>>,
-      required: true
-    },
+    // atividades: {
+    //   type: Array as PropType<Array<{ id: number; nome: string; descricao: string }>>,
+    //   required: true
+    // },
     projetos: {
       type: Array as PropType<Array<{ id: number; nome: string }>>
     }
@@ -138,29 +85,47 @@ export default defineComponent({
         nome: '',
         descricao: '',
         nomeProjeto: '',
+        // nomeProjeto: this.projeto?.nomeProjeto || 'Projeto 1',
         status: '',
         dataInicio: '',
         dataFim: ''
       }
     }
   },
+  // methods: {
+  //   salvarAtividade() {
+  //     this.$emit('novaAtividade', { ...this.novaAtividade })
+  //     this.novaAtividade.nome = ''
+  //     this.novaAtividade.descricao = ''
+  //     this.novaAtividade.nomeProjeto = ''
+  //     this.novaAtividade.status = ''
+  //     this.novaAtividade.dataInicio = ''
+  //     this.novaAtividade.dataFim = ''
+  //   }
+  // }
   methods: {
     salvarAtividade() {
-      // Lógica para salvar a nova atividade
-      this.$emit('novaAtividade', this.novaAtividade)
-      // Limpar os campos após salvar
-      this.novaAtividade.nome = ''
-      this.novaAtividade.descricao = ''
-      this.novaAtividade.nomeProjeto = ''
-      this.novaAtividade.status = ''
-      this.novaAtividade.dataInicio = ''
-      this.novaAtividade.dataFim = ''
+      this.$emit('novaAtividade', { ...this.novaAtividade })
+      this.$emit('close')
+      this.novaAtividade = {
+        descricao: '',
+        nomeProjeto: '',
+        status: '',
+        dataInicio: '',
+        dataFim: ''
+      }
     }
   }
 })
 </script>
 
 <style scoped>
+.close {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
 .teste {
   display: flex;
   gap: 2rem;
@@ -216,7 +181,7 @@ button:last-child {
   margin: 15% auto;
   padding: 20px;
   border: 1px solid #888;
-  width: 80%;
+  width: 50%;
 }
 
 .close {
@@ -243,15 +208,20 @@ label {
 }
 
 input[type='text'],
-textarea,
-select,
-input[type='date'] {
+textarea {
   width: 100%;
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
 }
 
+select,
+input[type='date'] {
+  width: 290px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
 .btn-salvar {
   background-color: #50bbce;
   color: #fff;
--- src/views/ProjetoDetalheView.vue
+++ src/views/ProjetoDetalheView.vue
@@ -1,14 +1,15 @@
 <template>
-    <div>
-      <h1>Detalhes do Projeto</h1>
+  <div>
+    <div class="task-detail">
+      <h1>Nome da Task</h1>
       <div v-if="project">
-        <p>ID: {{ project.id }}</p>
-        <p>Nome: {{ project.nome }}</p>
+        <!-- <p>ID: {{ project.id }}</p> -->
+        <p class="tag-project">{{ project.nome }}</p>
         <p>Cliente: {{ project.clienteNome }}</p>
         <p>Status: {{ project.status }}</p>
         <button @click="showActivities">Ver Atividades</button>
       </div>
-  
+
       <!-- Modal para mostrar detalhes das atividades -->
       <div v-if="showModal" class="modal">
         <div class="modal-content">
@@ -22,76 +23,99 @@
         </div>
       </div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  
-  export default defineComponent({
-    name: 'ProjetoDetalheView',
-    data() {
-      return {
-        project: null,
-        showModal: false
-      };
-    },
-    mounted() {
-      // Aqui você deve carregar os dados do projeto usando o ID passado na rota
-      const projectId = this.$route.params.id;
-      // Simulação de dados
-      this.project = {
-        id: projectId,
-        nome: 'Projeto Exemplo',
-        clienteNome: 'Cliente Exemplo',
-        status: 'Em Andamento',
-        atividades: [
-          { id: 1, nome: 'Atividade 1', descricao: 'Descrição da Atividade 1' },
-          { id: 2, nome: 'Atividade 2', descricao: 'Descrição da Atividade 2' }
-        ]
-      };
-    },
-    methods: {
-      showActivities() {
-        this.showModal = true;
-      }
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'ProjetoDetalheView',
+  data() {
+    return {
+      project: null,
+      showModal: false
     }
-  });
-  </script>
-  
-  <style scoped>
-  .modal {
-    display: block;
-    position: fixed;
-    z-index: 1;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100vh;
-    overflow: auto;
-    background-color: rgb(0, 0, 0);
-    background-color: rgba(0, 0, 0, 0.4);
+  },
+  mounted() {
+    // Aqui você deve carregar os dados do projeto usando o ID passado na rota
+    const projectId = this.$route.params.id
+    // Simulação de dados
+    this.project = {
+      id: projectId,
+      nome: 'Projeto 1',
+      clienteNome: 'Cliente Exemplo',
+      status: 'Em Andamento',
+      atividades: [
+        { id: 1, nome: 'Atividade 1', descricao: 'Descrição da Atividade 1' },
+        { id: 2, nome: 'Atividade 2', descricao: 'Descrição da Atividade 2' }
+      ]
+    }
+  },
+  methods: {
+    showActivities() {
+      this.showModal = true
+    }
   }
-  
-  .modal-content {
-    background-color: #fefefe;
-    margin: 15% auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
+})
+</script>
+
+<style scoped>
+.tag-project {
+  font-size: 12px;
+  background-color: #f4f7fe;
+  color: #414042;
+  width: 70px;
+  height: 25px;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  justify-content: center;
+}
+.task-detail {
+  background: white;
+  width: 312px;
+  padding: 20px;
+  margin: 100px;
+  border-radius: 10px;
+  h1 {
+    font-size: 18px;
+    color: #414042;
+    font-weight: 700;
   }
-  
-  .close {
-    color: #aaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-  }
-  
-  .close:hover,
-  .close:focus {
-    color: black;
-    text-decoration: none;
-    cursor: pointer;
-  }
-  </style>
-  
+}
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
